49e7a4af29274e6b7ea6f724648fcf79fb09dd1a
Ana Muraya <Anamuraya9@gmail.com>
2022-06-26 18:43:44 +0300

    Fixed undefined headers error

--- server/server.js
+++ server/server.js
@@ -13,35 +13,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/rbac")
-.then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/rbac").then(() => {
   console.log("Connected to the database successfully");
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(async (res, req, next) => {
-  if (res.headers["x-access-token"]) {
+app.use(async (req, res, next) => {
+  if (req.headers["x-access-token"]) {
     const accessToken = req.headers["x-access-token"];
     const { userId, exp } = await jwt.verify(
       accessToken,
       process.env.JWT_SECRET
     );
-
-    //checking if token has expired
+    // Check if token has expired
     if (exp < Date.now().valueOf() / 1000) {
-      return res
-        .status(401)
-        .json({
-          error: "JWT token has expired, to get a new one, please log in again",
-        });
+      return res.status(401).json({
+        error: "JWT token has expired, please login to obtain a new one",
+      });
     }
     res.locals.loggedInUser = await User.findById(userId);
     next();
   } else {
-    res.json({
-      message: "Token not available"
-    })
     next();
   }
 });
@@ -49,7 +42,5 @@ app.use("/", routes);
 app.listen(PORT, () => {
   console.log(`Server is listening on Port ${PORT}`);
 });
-
-
 
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MmI2MTVhNjA2YjYzMGY0NGJhN2M5NWEiLCJpYXQiOjE2NTYxMDAyNjIsImV4cCI6MTY1NjExODI2Mn0.EkNWquN2hga-tTy_HxrAnKIkap9l0jzdMfTe597LobY
